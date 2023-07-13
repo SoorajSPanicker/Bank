3e5007a0d5c386ecb08dfbfa8344c7a7e26c5839
unknown <srjspnckr1997@gmail.com>
2023-07-13 08:15:33 +0530

    data binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,12 +8,21 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   data="Happy Banking With Us... "
   pdata="Enter the account number"
+  acno:any=""
   // constructor(){ }
   // ngOnInit(): void {
 
   // }
   login(){
     alert("Login Clicked")
+  }
+  acnoChange(event:any){
+    // console.log(event.target.value);
+    this.acno=event.target.value
+    console.log(this.acno);
+    
+    
+
   }
 
 
