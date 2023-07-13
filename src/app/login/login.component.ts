b855ebbc2c4882ddd4398b4b6492bf83d3d59808
unknown <srjspnckr1997@gmail.com>
2023-07-13 10:45:04 +0530

    variable binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,12 +9,18 @@ export class LoginComponent {
   data="Happy Banking With Us... "
   pdata="Enter the account number"
   acno:any=""
+  psw:string=""
   // constructor(){ }
   // ngOnInit(): void {
 
   // }
-  login(){
-    alert("Login Clicked")
+  login(a:any,b:any){
+    this.acno=a.value
+    this.psw=b.value
+    console.log(a.value);
+    console.log(b.value);
+    
+    
   }
   acnoChange(event:any){
     // console.log(event.target.value);
