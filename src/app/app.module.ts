96bfde9070117464ecdab06a4edcaa17fa8f657b
unknown <srjspnckr1997@gmail.com>
2023-07-28 12:44:39 +0530

    transaction api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { HistoryComponent } from './history/history.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,7 @@ import {HttpClientModule} from '@angular/common/http';
     HttpClientModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,60 +2,98 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../service/data.service';
 import { FormBuilder, Validators } from '@angular/forms';
+import { DatePipe } from '@angular/common'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  user:any=""
-  acno:any
-  profileData:any={}
-  profileBal:any={}
+  user: any = '';
+  acno: any;
+  profileData: any = {};
+  profileBal: any = {};
+  message:any='';
+  status:any=true
   //MODEL FORM FOR MONEY TRANSFER
-  moneyTransferForm=this.fb.group({
-    toAcno:['',[Validators.required,Validators.pattern('[0-9]+')]],
-    amount:['',[Validators.required,Validators.pattern('[0-9]+')]],
-    psw:['',[Validators.required,Validators.pattern('[0-9a-zA-Z]+')]]
-  })
-  constructor(private rout:Router,private ds:DataService,private fb:FormBuilder){ }
+  moneyTransferForm = this.fb.group({
+    toAcno: ['', [Validators.required, Validators.pattern('[0-9]+')]],
+    amount: ['', [Validators.required, Validators.pattern('[0-9]+')]],
+    psw: ['', [Validators.required, Validators.pattern('[0-9a-zA-Z]+')]],
+  });
+  constructor(
+    private rout: Router,
+    private ds: DataService,
+    private fb: FormBuilder,
+    private datePipe:DatePipe
+  ) {}
   ngOnInit(): void {
-    if(localStorage.getItem("currentUname")){
-      this.user=localStorage.getItem("currentUname")
+    if (localStorage.getItem('currentUname')) {
+      this.user = localStorage.getItem('currentUname');
     }
   }
-  logout(){
-    this.rout.navigateByUrl("")
+  logout() {
+    this.rout.navigateByUrl('');
   }
 
-  profileView(){
-    if(localStorage.getItem("currentAcno")){
-      this.acno=localStorage.getItem("currentAcno")
+  profileView() {
+    if (localStorage.getItem('currentAcno')) {
+      this.acno = localStorage.getItem('currentAcno');
       console.log(this.acno);
-
     }
-    this.ds.getProfile(this.acno).subscribe((response:any)=>{
+    this.ds.getProfile(this.acno).subscribe((response: any) => {
       console.log(response);
-      this.profileData=response
-
-
-    })
+      this.profileData = response;
+    });
   }
-  balanceView(){
-    if(localStorage.getItem("currentAcno")){
-      this.acno=localStorage.getItem("currentAcno")
+  balanceView() {
+    if (localStorage.getItem('currentAcno')) {
+      this.acno = localStorage.getItem('currentAcno');
       console.log(this.acno);
-      
     }
-    this.ds.getBalance(this.acno).subscribe((response:any)=>{
+    this.ds.getBalance(this.acno).subscribe((response: any) => {
       console.log(response);
-      this.profileBal=response
-      
-    })
+      this.profileBal = response;
+    });
   }
-  
 
+  transfer() {
+    if (this.moneyTransferForm.valid) {
+      //from Acno
+      if (localStorage.getItem('currentAcno')) {
+        this.acno = localStorage.getItem('currentAcno');
+        console.log(this.acno);
+      }
+      var path = this.moneyTransferForm.value;
+      //toAcno
+      var toAcno = path.toAcno;
+      console.log(toAcno);
+      //psw
+      var psw = path.psw;
+      console.log(psw);
+      //amount
+      var amount = path.amount;
+      console.log(amount);
+      //date
+      var dateTime=new Date()
+      var dateData=this.datePipe.transform(dateTime,'short')
+      // console.log(dateData);
+      //api call
+      this.ds.moneyTransferApi(this.acno,toAcno,psw,amount,dateData)
+      .subscribe((result:any)=>{
+       this.message=result.message
+       this.status=true
+        
+      },
+      result=>{
+        this.message=result.error.message
+        this.status=false
+      })
+      
+    } else {
+      alert('Invalid form');
+      this.status=false
+    }
+  }
 }
-
-
